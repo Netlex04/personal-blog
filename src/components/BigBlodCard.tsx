09209c97d5c6d169c9d566f500b5966e66bd6ba8
Netlex04 <moritz.glueck4@gmail.com>
2023-10-22 21:37:57 +0200

    homepage almost done

---
+++ src/components/BigBlodCard.tsx
@@ -0,0 +1,21 @@
+const BigBlogCard = () => {
+    return ( 
+        <div className="card">
+            <div className="content-wrapper">
+                <p></p>
+                <h3></h3>
+                <img src="" alt="" />
+                <p></p>
+                <div className="flex-row space-between">
+                    <div className="flex-row space-between">
+                        <img src="" alt="" />
+                        <p className="small"></p>
+                    </div>
+                    <div className="content-wrapper"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default BigBlogCard;
---
+++ src/components/SmallBlogCard.tsx
@@ -0,0 +1,13 @@
+const SmallBlogCard = () => {
+    return (  
+        <div className="card">
+            <div className="content-wrapper">
+                <p className="brand-text"></p>
+                <h4 className="white-text"></h4>
+                <p className="bright-text"></p>
+            </div>
+        </div>
+    );
+}
+ 
+export default SmallBlogCard;
